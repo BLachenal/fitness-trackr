ae986d2d8142d57bac3ba4ad3206a5f5785097d2
Billy Lachenal <billylachenal@gmail.com>
2026-03-22 19:49:05 -0500

    finished all requirements

--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,14 +1,26 @@
 import { useAuth } from "../auth/AuthContext";
 import { deleteActivity } from "../api/activities";
 
-export default function ActivityList({ activities }) {
+export default function ActivityList({ activities, syncActivities }) {
   const {loggedIn} = useAuth();
   const {token} = useAuth();
 
+  //const tryCreateActivity = async (formData)
+  const tryDelete = async (activity) => {
+        try {
+          await deleteActivity(token, activity.id);
+          syncActivities();
+        } catch (e) {
+          console.error(e);
+        }
+  }
+//onClick={()=>tryDelete} formAction={()=>tryDelete}
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}{loggedIn ? (<button onClick={()=>deleteActivity(token, activity.id)}>Delete</button>) : ""}</li>
+        <li key={activity.id}>{activity.name}{
+          loggedIn ? (<button onClick={()=>tryDelete(activity)}>Delete</button>) : ""}
+          </li>
       ))}
     </ul>
   );
