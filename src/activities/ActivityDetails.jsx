f911c8e67cacd0c2c24043223424763dad40acf9
Billy Lachenal <billylachenal@gmail.com>
2026-03-23 22:13:46 -0500

    Completed BrowserRouter rework

---
+++ src/activities/ActivityDetails.jsx
@@ -0,0 +1,34 @@
+import { useParams, useNavigate} from "react-router";
+import {getAnActivity, deleteActivity} from "../api/activities";
+import { useEffect, useState } from "react";
+import { useAuth } from "../auth/AuthContext";
+
+
+export default function ActivityDetails(){
+    //{loggedIn ? (<button onClick={()=>tryDelete(activity)}>Delete</button>) : ""}
+    const [activity, setActivity] = useState();
+    const {token} = useAuth();
+    const {id} = useParams();
+    const nav = useNavigate();
+    useEffect(()=>{
+        const fetchActivity = async () =>{
+            setActivity(await getAnActivity(id));
+        }
+        fetchActivity();
+    }, []);
+
+    const handleClick = async () =>{
+        console.log(token, activity.id);
+        await deleteActivity(token, activity.id);
+        nav("/");
+    }    
+    return( 
+        <>
+            {activity && <>
+            <h1>{activity.name}</h1>
+            <h3>{activity.description}</h3>
+            <button onClick={handleClick}>X</button>
+            </>}
+        </>
+    );
+}
--- src/activities/ActivityList.jsx
+++ src/activities/ActivityList.jsx
@@ -1,26 +1,27 @@
 import { useAuth } from "../auth/AuthContext";
-import { deleteActivity } from "../api/activities";
+// import { deleteActivity } from "../api/activities";
+import { Link } from "react-router";
 
-export default function ActivityList({ activities, syncActivities }) {
+export default function ActivityList({ activities }) {
   const {loggedIn} = useAuth();
   const {token} = useAuth();
 
   //const tryCreateActivity = async (formData)
-  const tryDelete = async (activity) => {
+/*   const tryDelete = async (activity) => {
         try {
           await deleteActivity(token, activity.id);
           syncActivities();
         } catch (e) {
           console.error(e);
         }
-  }
+  } */
 //onClick={()=>tryDelete} formAction={()=>tryDelete}
   return (
     <ul>
       {activities.map((activity) => (
-        <li key={activity.id}>{activity.name}{
-          loggedIn ? (<button onClick={()=>tryDelete(activity)}>Delete</button>) : ""}
-          </li>
+        <li key={activity.id}>
+          <Link to ={"/activityDetail/" + activity.id}> {activity.name}</Link>
+        </li>
       ))}
     </ul>
   );
